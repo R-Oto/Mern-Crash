5528e67c9186dfca9de07e9ca6eeefceca92a3d7
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-05 13:17:03 +0400

    api completed

--- project-workouts/server/controllers/controller.js
+++ project-workouts/server/controllers/controller.js
@@ -1,41 +1,63 @@
-import Workout from "../models/model.js"
+import Workout from "../models/model.js";
 
 export const getAll = async (req, res) => {
-    try{
-
-    }catch(error){
-
+    try {
+        const workouts = await Workout.find();
+        res.status(200).json(workouts);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-}
+};
 
 export const createOne = async (req, res) => {
-    try{
-        const {title, load, reps} = req.body;
-    }catch(error){
-        
+    const { title, load, reps } = req.body;
+
+    try {
+        const workout = await Workout.create({ title, load, reps });
+        res.status(201).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-}
+};
 
 export const getOne = async (req, res) => {
-    try{
-
-    }catch(error){
-        
+    const id = req.params.id;
+    try {
+        const workout = await Workout.findById(id);
+        if (!workout) {
+            return res.status(404).json({ error: 'Workout not found' });
+        }
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-}
+};
 
 export const updateOne = async (req, res) => {
-    try{
+    const id = req.params.id;
+    const updates = req.body; 
 
-    }catch(error){
-        
+    try {
+        const workout = await Workout.findByIdAndUpdate(id, updates, { new: true }); 
+        if (!workout) {
+            return res.status(404).json({ error: 'Workout not found' }); 
+        }
+        res.status(200).json(workout); 
+    } catch (error) {
+        res.status(400).json({ error: error.message }); 
     }
-}
+};
+
 
 export const deleteOne = async (req, res) => {
-    try{
-
-    }catch(error){
-        
+    const id = req.params.id;
+    try {
+        const workout = await Workout.findByIdAndDelete(id);
+        if (!workout) {
+            return res.status(404).json({ error: 'Workout not found' });
+        }
+        res.status(200).json({ message: 'Workout deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-}
+};
